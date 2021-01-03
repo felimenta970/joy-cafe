7d43ad132f42d1580649cbe7c6cf321e76b67178
felimenta970 <felimenta97@gmail.com>
2021-01-03 20:46:11 -0300

    Desktop layout improvements v1

--- src/pages/almoco.js
+++ src/pages/almoco.js
@@ -4,7 +4,10 @@ import React from 'react';
 // Import icones
 import { FaBars } from 'react-icons/fa';
 
+import { Container, Row, Col } from "react-bootstrap";
+
 import almoco from './../assets/almoco-alt.jpg';
+import almocoBig from './../assets/almoco.jpg';
 
 // Import dados
 import { almocoItens } from "../data/dadosItens.js"; 
@@ -24,34 +27,46 @@ const Almoco = ({handleToggleSidebar}) => {
             <img src={almoco} alt="Foto de prato de comida" className="heroImgMobile" />
 
 
-            {/* Criamos um div para conter a lista de itens */}
-            <div className="allItens">
+            <Container fluid="true">
+            <Row>
+                <Col xs lg="7">
+                {/* Criamos um div para conter a lista de itens */}
+                    <div className="allItens">
 
 
-                {/* Do arquivo, pegamos os dados e uma chave para cada um */}
-                {almocoItens.map((data, key) => {
-                    return (
-                        {/* Exibimos o conteúdo e damos um nome de classe para cada um */},
-                        <div className="itemGroup" key={key}>
+                        {/* Do arquivo, pegamos os dados e uma chave para cada um */}
+                        {almocoItens.map((data, key) => {
+                            return (
+                                {/* Exibimos o conteúdo e damos um nome de classe para cada um */},
+                                <div className="itemGroup" key={key}>
 
-                            <div className="itemName">
-                                {data.itemName}
-                            </div>
+                                    <div className="itemName">
+                                        {data.itemName}
+                                    </div>
+                                    
 
+                                    <div className="itemDesc">
+                                        {data.itemDesc}
+                                    </div>
 
-                            <div className="itemDesc">
-                                {data.itemDesc}
-                            </div>
+                        
+                                    <div className="itemPreco">
+                                        {data.itemPreco}
+                                    </div>
 
-                            <div className="itemPreco">
-								{data.itemPreco}
-							</div>
+                                </div>
+                            );
+                        })}
+                        
+                    </div>
+                </Col>
 
-                        </div>
-                    );
-                })}
+                <Col lg="5">
+                <img src={almocoBig} alt="Foto de prato de comida" className="heroImg" />
+                </Col>
+            </Row>
 
-            </div>
+            </Container>
 
         </div>
     );
