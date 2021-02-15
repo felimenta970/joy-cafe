5ab3bc46291d7c1284ae579c238bcac6b69b439c
felimenta970 <felimenta97@gmail.com>
2021-02-15 18:52:57 -0300

    Added loading animation

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -4,6 +4,8 @@ const Loading = ({}) => {
     return (
         <div className="bgLoading">
             <h1 className="textLoading">Carregando</h1>
+
+            <div className="lds-ripple"><div></div><div></div></div>
         </div>
     )
 }
